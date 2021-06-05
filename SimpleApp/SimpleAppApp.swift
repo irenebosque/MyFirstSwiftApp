f77f8d0bea0b59e99c543ec17b446dd5731df914
irenebosque <ibosque@protonmail.com>
2021-06-05 22:39:57 +0200

    Initial Commit

---
+++ SimpleApp/SimpleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleAppApp.swift
+//  SimpleApp
+//
+//  Created by Irene Bosque on 05/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct SimpleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
